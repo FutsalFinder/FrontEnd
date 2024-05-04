50cc64f0c703cf004abbd6a565f52412deda5103
kimhongrae <khr0690@hanmail.net>
2024-05-04 20:31:57 +0900

    chore: 불필요한 파일 삭제

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -66,11 +66,9 @@ export const DataProvider: React.FC<{ children: ReactNode }> = ({
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
-
+      const url = `http://localhost:8080/matches/${date}?region=${region}`;
       try {
-        const response = await fetch(
-          `/api/getData?date=${date}&region=${region}`
-        );
+        const response = await fetch(url);
         if (!response.ok) throw new Error("Network Error");
         const rawData = await response.json();
         const transformedData = rawData.map((item: any) => ({
