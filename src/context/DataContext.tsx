c0fbaf5eaec5ce59dd2911831cd671b2851e3c86
kimhongrae <khr0690@hanmail.net>
2024-05-04 20:42:15 +0900

    fix: https로 변경

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -66,7 +66,7 @@ export const DataProvider: React.FC<{ children: ReactNode }> = ({
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
-      const url = `http://localhost:8080/matches/${date}?region=${region}`;
+      const url = `https://localhost:8081/matches/${date}?region=${region}`;
       try {
         const response = await fetch(url);
         if (!response.ok) throw new Error("Network Error");
