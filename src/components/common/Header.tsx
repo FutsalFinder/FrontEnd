144b07dff45b4e1d23c7df62aacaaa5467a2c7ac
kimhongrae <khr0690@hanmail.net>
2024-05-16 15:26:53 +0900

    style: 검색창 빼고 로고 가운데 정렬

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,14 +1,14 @@
 import styled from "styled-components";
-import { FiSearch } from "react-icons/fi";
+// import { FiSearch } from "react-icons/fi";
 
 const Header = () => {
   return (
     <HeaderStyle>
       <Title onClick={() => window.location.reload()}>Futsal Finder</Title>
-      <SearchInputContainer>
+      {/* <SearchInputContainer>
         <FiSearchIcon />
         <SearchInput type="search" placeholder="구장을 검색하세요." />
-      </SearchInputContainer>
+      </SearchInputContainer> */}
     </HeaderStyle>
   );
 };
@@ -21,27 +21,27 @@ const HeaderStyle = styled.div`
   justify-content: space-around;
 `;
 
-const SearchInputContainer = styled.div`
-  position: relative;
-`;
+// const SearchInputContainer = styled.div`
+//   position: relative;
+// `;
 
-const SearchInput = styled.input`
-  height: 40px;
-  width: 240px;
-  padding: 10px;
-  padding-left: 40px;
+// const SearchInput = styled.input`
+//   height: 40px;
+//   width: 240px;
+//   padding: 10px;
+//   padding-left: 40px;
 
-  @media screen and (max-width: 768px) {
-    width: 160px;
-  }
-`;
+//   @media screen and (max-width: 768px) {
+//     width: 160px;
+//   }
+// `;
 
-const FiSearchIcon = styled(FiSearch)`
-  position: absolute;
-  top: 50%;
-  left: 10px;
-  transform: translateY(-50%);
-`;
+// const FiSearchIcon = styled(FiSearch)`
+//   position: absolute;
+//   top: 50%;
+//   left: 10px;
+//   transform: translateY(-50%);
+// `;
 
 const Title = styled.h1`
   font-family: "Arial", sans-serif;
